604d56c042c620247ff7f9609a40991f99fc8b02
Fabian Mastenbroek <mail.fabianm@gmail.com>
2020-03-12 14:15:19 +0100

    Add entry point for server

--- htable-server/build.gradle.kts
+++ htable-server/build.gradle.kts
@@ -3,6 +3,11 @@ description = "Module for running HugeTable servers"
 /* Build configuration */
 plugins {
     `project-convention`
+    application
+}
+
+application {
+    mainClassName = "nl.tudelft.htable.server.Main"
 }
 
 dependencies {}
---
+++ htable-server/src/main/scala/nl/tudelft/htable/server/Main.scala
@@ -0,0 +1,16 @@
+package nl.tudelft.htable.server
+
+/**
+ * Main class of the HugeTable server program.
+ */
+object Main {
+
+  /**
+   * Main entry point of the program.
+   *
+   * @param args The command line arguments passed to the program.
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
